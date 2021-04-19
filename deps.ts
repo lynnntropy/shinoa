722a5c1ab1017da430c2e66c94804dfcf6b51884
Veselin Romić <omegavesko@gmail.com>
2021-04-19 13:09:27 +0200

    feat: initial boilerplate

--- deps.ts
+++ deps.ts
@@ -1,1 +1,2 @@
-export * from "https://deno.land/x/discordeno/mod.ts";
+export * as discordeno from "https://deno.land/x/discordeno/mod.ts";
+export * as log from "https://deno.land/std@0.93.0/log/mod.ts";
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,15 @@
-console.log("pog!");
+import { discordeno, log } from "../deps.ts";
+
+const token = Deno.env.get("TOKEN");
+if (!token) {
+  log.error("TOKEN variable is required.");
+  Deno.exit(1);
+}
+
+discordeno.startBot({
+  token,
+  intents: ["GUILDS", "GUILD_MESSAGES"],
+  eventHandlers: {
+    ready: () => console.log("Connected to Discord gateway!"),
+  },
+});

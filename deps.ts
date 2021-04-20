fe2c96e59642e05474aa0d0358d5b96266c99c81
Veselin Romić <omegavesko@gmail.com>
2021-04-20 17:42:07 +0200

    fix: lock discordeno version

--- deps.ts
+++ deps.ts
@@ -3,5 +3,5 @@ export * as log from "https://deno.land/std@0.93.0/log/mod.ts";
 export * as async from "https://deno.land/std@0.93.0/async/mod.ts";
 
 // Libraries
-export * as discordeno from "https://deno.land/x/discordeno/mod.ts";
+export * as discordeno from "https://deno.land/x/discordeno@10.5.0/mod.ts";
 export * as denodb from "https://deno.land/x/denodb@v1.0.33/mod.ts";

2badee590f619df598578d4e11016f875da1e6e5
Veselin Romić <omegavesko@gmail.com>
2021-04-21 01:11:15 +0200

    feat: nicer log output

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,10 @@
 import * as pino from "pino";
-const logger = pino({ prettyPrint: { colorize: true } });
+const logger = pino({
+  prettyPrint: {
+    colorize: true,
+    translateTime: "yyyy-mm-dd HH:MM",
+    ignore: "pid,hostname",
+  },
+});
 
 export default logger;

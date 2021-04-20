79f45c5e9c3ddaa09bae6367716a21b17edcf647
Veselin Romić <omegavesko@gmail.com>
2021-04-20 23:45:48 +0200

    feat: more stuff

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,27 @@
 import { getEnvironmentVariableOrThrow } from "./utils/config";
 
+interface GuildConfig {
+  [key: string]: { commands?: unknown };
+}
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const applicationId = "833659808187678771";
+
+const environment = {
+  TOKEN: getEnvironmentVariableOrThrow("TOKEN"),
+};
+
+const guilds: GuildConfig = {
+  // Vesko's Workshop
+  ["161167668131397642"]: {},
+};
+
 const config = {
-  environment: {
-    TOKEN: getEnvironmentVariableOrThrow("TOKEN"),
-  },
+  isProduction,
+  applicationId,
+  environment,
+  guilds,
 };
 
 export default config;
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,26 @@
 import logger from "./logger";
 import { Client } from "discord.js";
 import config from "./config";
+import { GatewayServer, SlashCreator } from "slash-create";
 
 const client = new Client();
 
+const slashCreator = new SlashCreator({
+  applicationID: config.applicationId,
+  token: config.environment.TOKEN,
+});
+
+slashCreator.withServer(
+  // @ts-ignore we know INTERACTION_CREATE is a valid event
+  new GatewayServer((handler) => client.ws.on("INTERACTION_CREATE", handler))
+);
+
+// TODO
+// slashCreator.registerCommands
+
 client.on("ready", () => {
   logger.info(
-    `Connected to Discord gateway as ${client.user.username}#${client.user.discriminator}!`
+    `Connected to Discord as ${client.user.username}#${client.user.discriminator}!`
   );
 });
 

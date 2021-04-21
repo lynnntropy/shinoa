b1f75bb82babee57726e6e5f134a17fe2f5ac5db
Veselin Romić <omegavesko@gmail.com>
2021-04-22 00:43:34 +0200

    feat: handle Discord rate limit headers

--- src/discord/api.ts
+++ src/discord/api.ts
@@ -14,6 +14,21 @@ import logger from "../logger";
 import { Command } from "../types";
 import { buildApplicationCommandBodyFromCommand } from "./utils";
 
+interface RateLimitState {
+  routeKeyBucketMappings: { [routeKey: string]: string };
+  buckets: {
+    [bucket: string]: {
+      remaining: number;
+      resetsAt: Date;
+    };
+  };
+}
+
+const rateLimitState: RateLimitState = {
+  routeKeyBucketMappings: {},
+  buckets: {},
+};
+
 const client = axios.create({
   baseURL: "https://discord.com/api/v8",
   headers: {
@@ -21,12 +36,53 @@ const client = axios.create({
   },
 });
 
-function sendDiscordAPIRequest<T = any, R = AxiosResponse<T>>(
+async function sendDiscordAPIRequest<T = any, R = AxiosResponse<T>>(
+  routeKey: string,
   config: AxiosRequestConfig
 ): Promise<R> {
   logger.debug(`(Discord) ${config.method} ${config.url}`);
   logger.trace(config);
-  return client.request(config);
+
+  const bucket = rateLimitState.routeKeyBucketMappings[routeKey];
+  const bucketState = rateLimitState.buckets[bucket];
+
+  if (
+    bucketState &&
+    bucketState.remaining <= 0 &&
+    bucketState.resetsAt > new Date()
+  ) {
+    const timeUntilResetMs =
+      bucketState.resetsAt.getTime() - new Date().getTime();
+
+    logger.warn(
+      `Hit rate limit for route key ${routeKey} (bucket ${bucket}). ` +
+        `Waiting ${timeUntilResetMs / 1000}s.`
+    );
+
+    await new Promise((yay) => setTimeout(yay, timeUntilResetMs));
+  }
+
+  const response = await client.request(config);
+
+  const rateLimitBucket = response.headers["x-ratelimit-bucket"];
+  const rateLimitRemaining = Number(response.headers["x-ratelimit-remaining"]);
+  const rateLimitResetsAt = new Date(
+    Number(response.headers["x-ratelimit-reset"]) * 1000
+  );
+
+  if (rateLimitBucket) {
+    rateLimitState.routeKeyBucketMappings[routeKey] = rateLimitBucket;
+    rateLimitState.buckets[rateLimitBucket] = {
+      remaining: rateLimitRemaining,
+      resetsAt: rateLimitResetsAt,
+    };
+
+    logger.trace(
+      `Rate limit bucket ${rateLimitBucket}: ${rateLimitRemaining} requests remaining`
+    );
+  }
+
+  return (response as unknown) as R;
 }
 
 export const registerCommand = async (
@@ -37,11 +93,15 @@ export const registerCommand = async (
     ? `/applications/${config.applicationId}/guilds/${guildId}/commands`
     : `/applications/${config.applicationId}/commands`;
 
+  const routeKey = guildId
+    ? "POST_/applications/:applicationId/guilds/:guildId/commands"
+    : "POST_/applications/:applicationId/commands";
+
   return (
     await sendDiscordAPIRequest<
       | RESTPostAPIApplicationCommandsResult
       | RESTPostAPIApplicationGuildCommandsResult
-    >({
+    >(routeKey, {
       method: "POST",
       url,
       data: buildApplicationCommandBodyFromCommand(command),
@@ -53,19 +113,25 @@ export const respondToInteraction = async (
   interaction: APIInteraction,
   payload: APIInteractionResponse
 ): Promise<void> => {
-  return await sendDiscordAPIRequest({
-    method: "POST",
-    url: `/interactions/${interaction.id}/${interaction.token}/callback`,
-    data: payload,
-  });
+  return await sendDiscordAPIRequest(
+    "POST_/interactions/:interactionId/:interactionToken/callback",
+    {
+      method: "POST",
+      url: `/interactions/${interaction.id}/${interaction.token}/callback`,
+      data: payload,
+    }
+  );
 };
 
 export const getGuildCommands = async (guildId: Snowflake) => {
   return (
-    await sendDiscordAPIRequest<RESTGetAPIApplicationGuildCommandsResult>({
-      method: "GET",
-      url: `/applications/${config.applicationId}/guilds/${guildId}/commands`,
-    })
+    await sendDiscordAPIRequest<RESTGetAPIApplicationGuildCommandsResult>(
+      "GET_/applications/:applicationId/guilds/:guildId/commands",
+      {
+        method: "GET",
+        url: `/applications/${config.applicationId}/guilds/${guildId}/commands`,
+      }
+    )
   ).data;
 };
 
@@ -73,24 +139,33 @@ export const deleteGuildCommand = async (
   guildId: Snowflake,
   commandId: Snowflake
 ) => {
-  return await sendDiscordAPIRequest({
-    method: "DELETE",
-    url: `/applications/${config.applicationId}/guilds/${guildId}/commands/${commandId}`,
-  });
+  return await sendDiscordAPIRequest(
+    "DELETE_/applications/:applicationId/guilds/:guildId/commands/:commandId",
+    {
+      method: "DELETE",
+      url: `/applications/${config.applicationId}/guilds/${guildId}/commands/${commandId}`,
+    }
+  );
 };
 
 export const getGlobalCommands = async () => {
   return (
-    await sendDiscordAPIRequest<RESTGetAPIApplicationCommandsResult>({
-      method: "GET",
-      url: `/applications/${config.applicationId}/commands`,
-    })
+    await sendDiscordAPIRequest<RESTGetAPIApplicationCommandsResult>(
+      "GET_/applications/:applicationId/commands",
+      {
+        method: "GET",
+        url: `/applications/${config.applicationId}/commands`,
+      }
+    )
   ).data;
 };
 
 export const deleteGlobalCommand = async (commandId: Snowflake) => {
-  return await sendDiscordAPIRequest({
-    method: "DELETE",
-    url: `/applications/${config.applicationId}/commands/${commandId}`,
-  });
+  return await sendDiscordAPIRequest(
+    "DELETE_/applications/:applicationId/commands/:commandId",
+    {
+      method: "DELETE",
+      url: `/applications/${config.applicationId}/commands/${commandId}`,
+    }
+  );
 };

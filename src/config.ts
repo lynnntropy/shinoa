8e5fa5e0162496d2a323a0207cf563a3de88beb3
Veselin Romić <omegavesko@gmail.com>
2021-04-22 11:00:46 +0200

    feat: refactor config

--- src/config.ts
+++ src/config.ts
@@ -3,33 +3,31 @@ import { commands as miscCommands } from "./modules/misc";
 import { commands as moderationCommands } from "./modules/moderation";
 import { Command } from "./types";
 
-interface GuildConfig {
-  [key: string]: { commands?: Command[] };
+interface Config {
+  ownerId: string;
+  applicationId: string;
+  isProduction: boolean;
+  globalCommands: Command[];
+  guilds: {
+    [key: string]: { commands?: Command[] };
+  };
 }
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const applicationId = "833659808187678771";
-
-const globalCommands: Command[] = [];
-
-const guilds: GuildConfig = {
-  // Vesko's Workshop
-  ["161167668131397642"]: {
-    commands: [
-      ...botAdministrationCommands,
-      ...miscCommands,
-      ...moderationCommands,
-    ],
-  },
-};
-
-const config = {
+const config: Config = {
   ownerId: "98225142064250880",
-  isProduction,
-  applicationId,
-  globalCommands,
-  guilds,
+  applicationId: "833659808187678771",
+  isProduction: process.env.NODE_ENV === "production",
+  globalCommands: [],
+  guilds: {
+    // Vesko's Workshop
+    ["161167668131397642"]: {
+      commands: [
+        ...botAdministrationCommands,
+        ...miscCommands,
+        ...moderationCommands,
+      ],
+    },
+  },
 };
 
 export default config;

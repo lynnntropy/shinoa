afeb2f445b0d7691a83d7139cda24b2de1c35aac
Veselin Romić <omegavesko@gmail.com>
2021-04-22 14:04:44 +0200

    feat: remove TypeORM stuff

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,8 @@
-import "reflect-metadata";
 import logger from "./logger";
 import environment from "./environment";
 import client from "./client";
-import initializeDatabase from "./database";
 
 const main = async () => {
-  await initializeDatabase();
-
   logger.info("Starting client...");
   await client.login(environment.TOKEN);
 };

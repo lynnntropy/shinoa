e73114722d73bfa196ce3f9b74b27ed639175bc9
Veselin Romić <omegavesko@gmail.com>
2021-05-04 00:11:05 +0200

    feat: add /eval

--- src/modules/BotAdministrationModule.ts
+++ src/modules/BotAdministrationModule.ts
@@ -40,8 +40,33 @@ class SayCommand implements Command {
   }
 }
 
+class EvalCommand implements Command {
+  name = "eval";
+  description = "Runs arbitrary JavaScript (obviously owner-only).";
+  isOwnerOnly = true;
+  options: APIApplicationCommandOption[] = [
+    {
+      name: "input",
+      description: "The code to run.",
+      type: ApplicationCommandOptionType.STRING,
+      required: true,
+    },
+  ];
+
+  async handle(interaction: APIInteraction) {
+    const input = (interaction.data
+      .options[0] as ApplicationCommandInteractionDataOptionString).value;
+    const output = eval(input);
+
+    await respondToInteraction(interaction, {
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: { content: `\`\`\`\n${output}\n\`\`\`` },
+    });
+  }
+}
+
 const BotAdministrationModule: Module = {
-  commands: [new SayCommand()],
+  commands: [new SayCommand(), new EvalCommand()],
   handlers: {},
 };
 

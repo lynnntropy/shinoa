a50cd8f150d2d109fb7f9a4c75a567a7d7b58769
Veselin Romić <omegavesko@gmail.com>
2021-10-04 16:05:37 +0200

    feat: Sentry

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import logger from "./logger";
 import environment from "./environment";
 import client from "./client";
 import { server as graphqlServer } from "./graphql";
+import * as Sentry from "@sentry/node";
+import "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://a16130edcd654fe2a89fe58ab5315a6f@o345514.ingest.sentry.io/5991672",
+  tracesSampleRate: 1.0,
+});
 
 const main = async () => {
   logger.info("Starting client...");

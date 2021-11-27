e6b48df46704f8370781e82cdbdb3f790b51353e
Veselin Romić <omegavesko@gmail.com>
2021-11-27 17:23:41 +0100

    feat: closing and reopening polls

--- src/modules/PollModule.ts
+++ src/modules/PollModule.ts
@@ -1,10 +1,13 @@
+import { Poll } from ".prisma/client";
 import { bold, hyperlink } from "@discordjs/builders";
 import {
   GuildChannel,
   MessageActionRow,
+  MessageOptions,
   MessageSelectMenu,
   MessageSelectOptionData,
   PermissionResolvable,
+  TextChannel,
 } from "discord.js";
 import { Command, CommandSubCommand } from "../internal/command";
 import { Module } from "../internal/types";
@@ -70,6 +73,14 @@ class PollCommand extends Command {
         ) as GuildChannel;
         const rawOptions = interaction.options.getString("options", true);
 
+        if (!interaction.inGuild()) {
+          interaction.reply({
+            content: `Polls can only be managed in a server.`,
+            ephemeral: true,
+          });
+          return;
+        }
+
         if (!channel.isText()) {
           interaction.reply({
             content: `Channel must be a text channel.`,
@@ -78,7 +89,11 @@ class PollCommand extends Command {
           return;
         }
 
-        const existingPoll = await prisma.poll.findUnique({ where: { id } });
+        const existingPoll = await prisma.poll.findUnique({
+          where: {
+            guildId_localId: { guildId: interaction.guildId, localId: id },
+          },
+        });
         if (existingPoll !== null) {
           interaction.reply({
             content: `A poll with the ID \`${id}\` already exists.`,
@@ -103,24 +118,16 @@ class PollCommand extends Command {
           throw e;
         }
 
-        const actionRow = new MessageActionRow().addComponents(
-          new MessageSelectMenu()
-            .setCustomId(id)
-            .setPlaceholder("Select an option to cast your vote.")
-            .addOptions(options)
-        );
-
-        const message = await channel.send({
-          content: `Poll: ${bold(name)}`,
-          components: [actionRow],
-        });
+        const message = await channel.send(buildPollMessage(id, name, options));
 
         await prisma.poll.create({
           data: {
-            id,
             guildId: channel.guildId,
+            localId: id,
+            name,
             channelId: channel.id,
             messsageId: message.id,
+            options: options as any[],
           },
         });
 
@@ -138,17 +145,154 @@ class PollCommand extends Command {
     {
       name: "close",
       description: "Close an active poll.",
+      options: [
+        {
+          type: "STRING",
+          name: "id",
+          description: "The ID of the poll.",
+          required: true,
+        },
+      ],
 
-      async handle(interaction) {},
+      async handle(interaction) {
+        const id = interaction.options.getString("id", true);
+
+        if (!interaction.inGuild()) {
+          interaction.reply({
+            content: `Polls can only be managed in a server.`,
+            ephemeral: true,
+          });
+          return;
+        }
+
+        const poll = await prisma.poll.findUnique({
+          where: {
+            guildId_localId: { guildId: interaction.guildId, localId: id },
+          },
+        });
+        if (poll === null) {
+          await interaction.reply({
+            content: "There's no poll with that ID.",
+            ephemeral: true,
+          });
+          return;
+        }
+
+        if (!poll.active) {
+          await interaction.reply({
+            content: `Poll ${bold(poll.name)} is already closed.`,
+            ephemeral: true,
+          });
+          return;
+        }
+
+        await prisma.poll.update({
+          where: { id: poll.id },
+          data: { active: false },
+        });
+
+        const channel = (await interaction.guild?.channels.fetch(
+          poll.channelId
+        )) as TextChannel;
+        const message = await channel.messages.fetch(poll.messsageId);
+        await message.edit({
+          content: `Poll ${bold(poll.name)} has been closed.`,
+          components: [],
+        });
+
+        await interaction.reply({
+          content: `Poll ${bold(poll.name)} successfully closed.`,
+        });
+      },
     },
     {
       name: "reopen",
       description: "Reopen a previously closed poll.",
+      options: [
+        {
+          type: "STRING",
+          name: "id",
+          description: "The ID of the poll.",
+          required: true,
+        },
+      ],
 
-      async handle(interaction) {},
+      async handle(interaction) {
+        const id = interaction.options.getString("id", true);
+
+        if (!interaction.inGuild()) {
+          interaction.reply({
+            content: `Polls can only be managed in a server.`,
+            ephemeral: true,
+          });
+          return;
+        }
+
+        const poll = await prisma.poll.findUnique({
+          where: {
+            guildId_localId: { guildId: interaction.guildId, localId: id },
+          },
+        });
+        if (poll === null) {
+          await interaction.reply({
+            content: "There's no poll with that ID.",
+            ephemeral: true,
+          });
+          return;
+        }
+
+        if (poll.active) {
+          await interaction.reply({
+            content: `Poll ${bold(poll.name)} is already open.`,
+            ephemeral: true,
+          });
+          return;
+        }
+
+        await prisma.poll.update({
+          where: { id: poll.id },
+          data: { active: true },
+        });
+
+        const channel = (await interaction.guild?.channels.fetch(
+          poll.channelId
+        )) as TextChannel;
+        const message = await channel.messages.fetch(poll.messsageId);
+        await message.edit(
+          buildPollMessage(
+            poll.localId,
+            poll.name,
+            poll.options as unknown as MessageSelectOptionData[]
+          )
+        );
+
+        await interaction.reply({
+          content: `Poll ${bold(poll.name)} successfully reopened.`,
+        });
+      },
     },
   ];
 }
+
+const buildPollMessage = (
+  localId: string,
+  name: string,
+  options: MessageSelectOptionData[]
+): MessageOptions => {
+  const actionRow = new MessageActionRow().addComponents(
+    new MessageSelectMenu()
+      .setCustomId(localId)
+      .setPlaceholder("Select an option to cast your vote.")
+      .addOptions(options)
+  );
+
+  const message: MessageOptions = {
+    content: `Poll: ${bold(name)}`,
+    components: [actionRow],
+  };
+
+  return message;
+};
 
 // TODO listen for incoming votes
 // probably best to do this by remembering the message ID for each poll

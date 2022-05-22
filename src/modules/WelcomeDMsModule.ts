27c35290a5bc3f964ee3373db0400cec71537d71
Lynn Romich <omegavesko@gmail.com>
2022-05-22 20:48:20 +0200

    feat: welcome DMs (#85)

---
+++ src/modules/WelcomeDMsModule.ts
@@ -0,0 +1,29 @@
+import { Guild, GuildMember, MessageOptions } from "discord.js";
+import config from "../config";
+import { EventHandler, Module } from "../internal/types";
+
+export type GuildWelcomeDMsConfig = {
+  enabled: true;
+  messageBuilder: (guild: Guild, member: GuildMember) => MessageOptions;
+};
+
+const handleGuildMemberAdd: EventHandler<"guildMemberAdd"> = async (member) => {
+  const guildConfig = config.guilds[member.guild.id].welcomeDMs;
+
+  if (!guildConfig?.enabled) {
+    return;
+  }
+
+  const message = guildConfig.messageBuilder(member.guild, member);
+
+  await member.send(message);
+};
+
+const WelcomeDMsModule: Module = {
+  commands: [],
+  handlers: {
+    guildMemberAdd: [handleGuildMemberAdd],
+  },
+};
+
+export default WelcomeDMsModule;
